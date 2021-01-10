a8353cb866c194479aa46468e7db1b022f014ceb
GQiangQ <740407980@qq.com>
2021-01-10 22:52:55 +0800

    add posts table

---
+++ src/migration/1610288581720-CreactPost.ts
@@ -0,0 +1,24 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreactPost1610288581720 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // 升级数据库的
+        return await queryRunner.createTable(
+          new Table({
+              name: 'posts',
+              columns: [
+                  { name: 'id', type: 'int', isPrimary: true, isGenerated: true, generationStrategy: "increment" },
+                  { name: 'title', type: 'varchar', },
+                  { name: 'content', type: 'text' }
+              ]
+          })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        // 降级数sssssssssssss据库的
+      return await queryRunner.dropTable('posts')
+    }
+
+}
