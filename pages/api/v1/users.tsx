f9483312effaaaaf154dd367b14fd869a0aa2570
GQiangQ <740407980@qq.com>
2021-02-10 22:32:01 +0800

    展示blog

---
+++ pages/api/v1/users.tsx
@@ -0,0 +1,27 @@
+import {NextApiHandler} from 'next';
+import {getDatabaseConnection} from '../../../lib/getDatabaseConnection';
+import {User} from '../../../src/entity/User';
+import md5 from 'md5';
+
+const Posts: NextApiHandler = async (req, res) => {
+  const {username, password, passwordConfirmation} = req.body;
+  const connection = await getDatabaseConnection();// 第一次链接能不能用 get
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+  const user = new User();
+  user.username = username.trim();
+  user.password = `${md5(password)}q`;
+  // user.passwordConfirmation = passwordConfirmation;
+  // await user.validate();
+  // if (user.hasErrors()) {
+  //   res.statusCode = 422;
+  //   res.write(JSON.stringify(user.errors));
+  // } else {
+  //   await connection.manager.save(user);
+  //   res.statusCode = 200;
+  //   res.write(JSON.stringify(user));
+  // }
+  res.end();
+};
+
+export default Posts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
-import {GetServerSideProps, NextPage} from 'next';
+import {GetServerSideProps, NextPage,} from 'next';
 import {UAParser} from 'ua-parser-js';
 import {useEffect, useState} from 'react';
+import { createConnection } from 'typeorm';
+import { getDatabaseConnection } from '../lib/getDatabaseConnection'
+import { Post } from 'src/entity/Post';
+import day from 'dayjs'
+import Link from 'next/link';
 
 type Props = {
+  posts: Post[],
   browser: {
     name: string;
     version: string;
@@ -10,27 +16,47 @@ type Props = {
   }
 }
 const index: NextPage<Props> = (props) => {
-  const {browser} = props;
+  const {browser, posts} = props;
+  console.log(posts)
   const [width, setWidth] = useState(0);
   useEffect(() => {
     const w = document.documentElement.clientWidth;
     setWidth(w);
   }, []);
+  console.log()
+  const date = (str:Date) =>day(str).format('YYYY-MM-DD: HH:mm')
   return (
     <div>
-      <h1>你的浏览器是 {browser.name}</h1>
-      <h2>你的浏览器窗口大小是 {width} 像素</h2>
+      {posts.map(post=>
+        <div key={post.id}>
+          <Link  href={`/posts/${post.id}`}>
+            <a>
+              <div>{post.title}</div>
+            </a>
+          </Link>
+          <div>{post.content}</div>
+          <div>
+            创建时间：{date(post.createdAt)}
+          </div>
+        </div>
+        
+      )
+      }
     </div>
   );
 };
 export default index;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const connect = await getDatabaseConnection()
+  let posts = await connect.manager.find(Post)
+  posts = JSON.parse(JSON.stringify(posts))
   const ua = context.req.headers['user-agent'];
   const result = new UAParser(ua).getResult();
   return {
     props: {
-      browser: result.browser
+      browser: result.browser,
+      posts
     }
   };
 };
